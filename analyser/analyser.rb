967331d83e0c33dee01ce5a9b5ce4c417460314a
Michael Morris <mtcmorris@gmail.com>
2017-06-24 23:10:12 +1000

    Add simple classifier to match result screen

--- analyser/analyser.rb
+++ analyser/analyser.rb
@@ -1,6 +1,7 @@
 require './screenshot'
 require './screens/race_screen'
 require './screens/race_result_screen'
+require './screens/match_result_screen'
 require './screens/loading_screen'
 
 class Analyser
@@ -51,7 +52,8 @@ class Analyser
     [
       LoadingScreen,
       RaceScreen,
-      RaceResultScreen
+      RaceResultScreen,
+      MatchResultScreen
     ]
   end
 end
