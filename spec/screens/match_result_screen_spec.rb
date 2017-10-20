76ad30789b4dbad0a9110be9d2e50ce17df94292
Michael Morris <mtcmorris@gmail.com>
2017-10-20 16:03:53 +1100

    Add 200cc support

--- spec/screens/match_result_screen_spec.rb
+++ spec/screens/match_result_screen_spec.rb
@@ -8,11 +8,28 @@ describe MatchResultScreen do
 
     it 'should extract all player positions correctly' do
       is_expected.to eq({
+        speed: '150cc',
         player_one: {position: 8},
         player_three: {position: 9},
         player_two: {position: 11},
         player_four: {position: 4}
       })
+    end
+  end
+
+  describe 'matches_image?' do
+    subject { described_class.matches_image?(screenshot) }
+
+    context 'for 150cc' do
+      let(:screenshot) { Screenshot.new(fixture('match-result.jpg')) }
+
+      it { is_expected.to be true }
+    end
+
+    context 'for 200cc' do
+      let(:screenshot) { Screenshot.new(fixture('match-result200cc.jpg')) }
+
+      it { is_expected.to be true }
     end
   end
 end
