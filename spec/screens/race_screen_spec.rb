787e1f93a65bf67cfdcdbc6f9fecd56410693137
Michael Morris <mtcmorris@gmail.com>
2018-03-06 13:27:34 +1100

    Spec all 4 players having items

--- spec/screens/race_screen_spec.rb
+++ spec/screens/race_screen_spec.rb
@@ -12,5 +12,18 @@ describe RaceScreen do
         player_two: {position: 6, item: "boomerang"}
       })
     end
+
+    context 'when all players have items' do
+      let(:image) { Screenshot.new(fixture('race-items.jpg')) }
+
+      it 'should detect the items' do
+        is_expected.to eq({
+          :player_four => {:position=>1, :item=>"banana"},
+          :player_one => {:position=>7, :item=>"fire-flower"},
+          :player_three => {:position=>3, :item=>"green-shell"},
+          :player_two => {:position=>10, :item=>"green-shell"}
+        })
+      end
+    end
   end
 end
