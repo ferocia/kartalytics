78074beb3cb087b14f2c561fa2ddbd025cd162c0
Christian Varga <christian.varga@ferocia.com.au>
2022-08-22 08:40:44 +1000

    analyse scores on match result screen
    allows the final scores to be corrected if an individual race results screen is missed

--- analyser/spec/screens/match_result_screen_spec.rb
+++ analyser/spec/screens/match_result_screen_spec.rb
@@ -2,18 +2,36 @@ require 'spec_helper'
 
 describe MatchResultScreen do
   describe ".extract_event" do
-    let(:screenshot) { Screenshot.new(fixture('match-result.jpg')) }
+    context 'for 150cc' do
+      let(:screenshot) { Screenshot.new(fixture('match-result.jpg')) }
 
-    subject { described_class.extract_event(screenshot)[:data] }
+      subject { described_class.extract_event(screenshot)[:data] }
 
-    it 'should extract all player positions correctly' do
-      is_expected.to eq({
-        speed: '150cc',
-        player_one: {position: 8},
-        player_three: {position: 9},
-        player_two: {position: 11},
-        player_four: {position: 4}
-      })
+      it 'extracts all player positions and scores correctly' do
+        is_expected.to eq({
+          speed: '150cc',
+          player_one: { position: 8, score: 21 },
+          player_three: { position: 9, score: 21 },
+          player_two: { position: 11, score: 16 },
+          player_four: { position: 4, score: 23 },
+        })
+      end
+    end
+
+    context 'for 200cc' do
+      let(:screenshot) { Screenshot.new(fixture('match-result200cc.jpg')) }
+
+      subject { described_class.extract_event(screenshot)[:data] }
+
+      it 'extracts all player positions and scores correctly' do
+        is_expected.to eq({
+          speed: '200cc',
+          player_one: { position: 1, score: 68 },
+          player_three: { position: 2, score: 68 },
+          player_two: { position: 3, score: 64 },
+          player_four: { position: 4, score: 60 },
+        })
+      end
     end
   end
 
